3233d2693a8dcad541a8a83a87af5dcedc1ca76f
Guorui Zheng <153406774+Brother-Rui@users.noreply.github.com>
2024-10-15 14:59:02 +0800

    Update 1.data_process_test&dev.sh

--- 1.data_process_test&dev.sh
+++ 1.data_process_test&dev.sh
@@ -1,15 +1,15 @@
-#Qwen2
- python ./src/prepare/data_process_test_qwen2.py \
+#Qwen2 Major and Minor Test Data
+python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/test.json \
  --few_shot 3 \
  --save_path ./data/Qwen2-7B_Prodata/test.json
 
- python ./src/prepare/data_process_test_qwen2.py \
+python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/dev.json \
  --few_shot 3 \
  --save_path ./data/Qwen2-7B_Prodata/dev.json
-
- python ./src/prepare/data_process_test_qwen2.py \
+ 
+python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/minor_test.json \
  --few_shot 3 \
  --save_path ./data/Qwen2-7B_Prodata/test.json
