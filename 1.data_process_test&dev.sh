925f8fb2c71c2ce3c4a9e70ed2cf91c45f710b33
Guorui Zheng <153406774+Brother-Rui@users.noreply.github.com>
2024-10-15 15:06:27 +0800

    Update 1.data_process_test&dev.sh

--- 1.data_process_test&dev.sh
+++ 1.data_process_test&dev.sh
@@ -1,16 +1,16 @@
-#Qwen2 Major and Minor Test Data
+# Major and Minor Test Data for Apollo2 trained with Qwen2
 python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/test.json \
  --few_shot 3 \
- --save_path ./data/Qwen2-7B_Prodata/test.json
+ --save_path ./data/Apollo2-7B_Prodata/test.json
 
 python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/dev.json \
  --few_shot 3 \
- --save_path ./data/Qwen2-7B_Prodata/dev.json
+ --save_path ./data/Apollo2-7B_Prodata/dev.json
  
 python ./src/prepare/data_process_test_qwen2.py \
  --data_path ./metadata/minor_test.json \
  --few_shot 3 \
- --save_path ./data/Qwen2-7B_Prodata/test.json
+ --save_path ./data/Apollo2-7B_Prodata/test.json
 
