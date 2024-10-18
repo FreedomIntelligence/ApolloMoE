6f7bf16e6617202af6e2bbfc9e08a1223d7729af
Guorui Zheng <153406774+Brother-Rui@users.noreply.github.com>
2024-10-18 15:18:09 +0800

    Add files via upload

---
+++ 0.extract_data.sh
@@ -0,0 +1,10 @@
+
+python metadata/extract_dataset.py \
+--dataset_path /path/to/ApolloMoEDataset.json \
+--major_dataset_path /path/to/MajorDataset.json \
+--minor_dataset_path /path/to/MinorDataset.json
+
+python metadata/extract_bench.py \
+--bench_path /path/to/ApolloMoEBench.json \
+--major_bench_path /path/to/MajorBench.json \
+--minor_bench_path /path/to/MinorBench.json
